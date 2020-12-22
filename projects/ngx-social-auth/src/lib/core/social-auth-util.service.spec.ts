e42f11d889dc363c533f041044e7f9c3ad970581
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-22 20:02:03 +0200

    test: unit test for SocialAuthUtilService

---
+++ projects/ngx-social-auth/src/lib/core/social-auth-util.service.spec.ts
@@ -0,0 +1,21 @@
+import {TestBed} from '@angular/core/testing';
+import {SocialAuthUtilService} from './social-auth-util.service';
+
+describe('SocialAuthUtilService', () => {
+  let service: SocialAuthUtilService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        SocialAuthUtilService
+      ],
+    });
+
+    service = TestBed.inject(SocialAuthUtilService);
+  });
+
+  it('should create', () => {
+    expect(service).toBeTruthy();
+  });
+});
+
