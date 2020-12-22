fb853c22153e4dd75e2edb8a71e0d05cd472a092
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-22 17:20:09 +0200

    feat: prevent reimport of the NgxSocialAuthModule

--- projects/ngx-social-auth/src/lib/social-auth.module.ts
+++ projects/ngx-social-auth/src/lib/social-auth.module.ts
@@ -1,4 +1,4 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
 import {NgxSocialAuthModuleConfig} from './social-auth-module-config';
 import {fromSocialAuthProviders} from './core/social-auth-providers';
 
@@ -8,6 +8,14 @@ import {fromSocialAuthProviders} from './core/social-auth-providers';
  */
 @NgModule()
 export class NgxSocialAuthModule {
+
+  constructor(@Optional() @SkipSelf() parentModule?: NgxSocialAuthModule) {
+    if (parentModule) {
+      throw new Error(
+        'NgxSocialAuthModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
   static forRoot(config: NgxSocialAuthModuleConfig): ModuleWithProviders<NgxSocialAuthModule> {
     const socialAuthProviders = config.providers ?? [];
     const providers = fromSocialAuthProviders(socialAuthProviders);
