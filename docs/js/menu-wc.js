1ae37e57b6239d5a6eae22ee8829ef6f9e20db1a
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-24 04:42:53 +0200

    docs: build docs site

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -16,7 +16,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">ngx-social-auth documentation</a>
+                    <a href="index.html" data-type="index-link">ngx-social-auth2 documentation</a>
                 </li>
 
                 <li class="divider"></li>
