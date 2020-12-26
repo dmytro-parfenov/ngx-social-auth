af38bf7d8ce252e246634314648d0b7a08d63b42
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-26 16:17:44 +0200

    refactor(demo): material theming

--- projects/ngx-social-auth-demo/src/app/shared/auth-response-bottom-sheet/auth-response-bottom-sheet.component.ts
+++ projects/ngx-social-auth-demo/src/app/shared/auth-response-bottom-sheet/auth-response-bottom-sheet.component.ts
@@ -1,8 +1,8 @@
 import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
-import {MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
 import {NgxSocialAuthProviderType, NgxSocialAuthResponse} from 'ngx-social-auth2';
 import {AppAuthResponseBottomSheetData} from './app-auth-response-bottom-sheet-data';
 import {ProviderNameService} from '../../core/provider-name.service';
+import {MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-auth-response-bottom-sheet',
@@ -13,22 +13,18 @@ import {ProviderNameService} from '../../core/provider-name.service';
 export class AuthResponseBottomSheetComponent {
 
   get authResponse(): NgxSocialAuthResponse {
-    return this.data.authResponse;
+    return this.data?.authResponse ?? null;
   }
 
-  get type(): NgxSocialAuthProviderType {
-    return this.data.providerType;
+  get type(): NgxSocialAuthProviderType | null {
+    return this.data?.providerType ?? null;
   }
 
   get providerName(): string {
-    return this.providerNameService.resolve(this.type) ?? '-';
+    return this.type ? this.providerNameService.resolve(this.type) : '-';
   }
 
-  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) private readonly data: AppAuthResponseBottomSheetData,
-              private readonly providerNameService: ProviderNameService) {
-    if (!this.data) {
-      throw new Error('AuthResponseBottomSheetComponent must contain MAT_BOTTOM_SHEET_DATA');
-    }
-  }
+  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) private readonly data: AppAuthResponseBottomSheetData | null,
+              private readonly providerNameService: ProviderNameService) {}
 
 }
