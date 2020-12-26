8909fab9a790253c81d30bfa12605d38c6c2b06e
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-27 00:25:23 +0200

    build(demo): change google prod environment

--- projects/ngx-social-auth-demo/src/environments/environment.prod.ts
+++ projects/ngx-social-auth-demo/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleClientId: '795988813952-4d4e4qcbseg2dlqulhp3c35tda8hp3om.apps.googleusercontent.com',
+  googleClientId: '422761057421-32ts59dkd6moeqbelbkk44oh00jkf88b.apps.googleusercontent.com',
   facebookAppId: '1119074078473230',
   microsoftClientId: '3e46b8b5-7756-452c-b6b2-d744f7fe6056',
   microsoftPostLogoutRedirect: 'https://dmytro-parfenov.github.io/ngx-social-auth-demo/'
