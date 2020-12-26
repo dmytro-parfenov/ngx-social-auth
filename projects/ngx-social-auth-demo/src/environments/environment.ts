121287ec1f5445a926f8d1f2b6525b8199340877
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-26 23:58:12 +0200

    build(demo): change facebook dev environment

--- projects/ngx-social-auth-demo/src/environments/environment.ts
+++ projects/ngx-social-auth-demo/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   googleClientId: '795988813952-4d4e4qcbseg2dlqulhp3c35tda8hp3om.apps.googleusercontent.com',
-  facebookAppId: '1119074078473230',
+  facebookAppId: '513909279524306',
   microsoftClientId: '3e46b8b5-7756-452c-b6b2-d744f7fe6056',
   microsoftPostLogoutRedirect: 'http://localhost:4200/'
 };
