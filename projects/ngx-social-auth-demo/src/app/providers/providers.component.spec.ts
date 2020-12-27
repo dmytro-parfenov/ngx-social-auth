523fe967021c04e26698aeee3ec469e11b9e9f95
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-27 13:15:00 +0200

    test(demo): test for ProvidersComponent

--- projects/ngx-social-auth-demo/src/app/providers/providers.component.spec.ts
+++ projects/ngx-social-auth-demo/src/app/providers/providers.component.spec.ts
@@ -1,26 +1,55 @@
-/*
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {AppComponent} from './app.component';
 import {NgxSocialAuthProviderType, NgxSocialAuthService} from 'ngx-social-auth2';
-import {EMPTY} from 'rxjs';
+import {EMPTY, of} from 'rxjs';
+import {ProvidersComponent} from './providers.component';
+import {MatBottomSheet, MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {ToastrModule} from 'ngx-toastr';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {authModuleConfig} from '../auth-module-config';
 
-describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
+@Component({selector: 'app-provider', template: ''})
+class ProviderStubComponent {
+  @Input() type: NgxSocialAuthProviderType | null = null;
+
+  @Input() isAuthorized = false;
+
+  @Input() isPending = true;
+
+  @Output() signIn = new EventEmitter<void>();
+
+  @Output() signOut = new EventEmitter<void>();
+
+  @Output() showAuthResponse = new EventEmitter<void>();
+}
+
+describe('ProvidersComponent', () => {
+  let component: ProvidersComponent;
+  let fixture: ComponentFixture<ProvidersComponent>;
   let socialAuthServiceSpy: jasmine.SpyObj<NgxSocialAuthService>;
+  let matBottomSheetSpy: jasmine.SpyObj<MatBottomSheet>;
 
   beforeEach(async () => {
     const socialAuthService = jasmine.createSpyObj('NgxSocialAuthService', ['signIn', 'signOut', 'getState']);
+    const matBottomSheet = jasmine.createSpyObj('MatBottomSheet', ['open']);
 
     await TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
+      imports: [
+        MatBottomSheetModule,
+        ToastrModule.forRoot()
+      ],
+      declarations: [
+        ProvidersComponent,
+        ProviderStubComponent
+      ],
       providers: [
-        {provide: NgxSocialAuthService, useValue: socialAuthService}
+        {provide: NgxSocialAuthService, useValue: socialAuthService},
+        {provide: MatBottomSheet, useValue: matBottomSheet}
       ]
     })
       .compileComponents();
 
     socialAuthServiceSpy = TestBed.inject(NgxSocialAuthService) as jasmine.SpyObj<NgxSocialAuthService>;
+    matBottomSheetSpy = TestBed.inject(MatBottomSheet) as jasmine.SpyObj<MatBottomSheet>;
 
     socialAuthServiceSpy.signIn.and.returnValue(EMPTY);
     socialAuthServiceSpy.signOut.and.returnValue(EMPTY);
@@ -28,17 +57,27 @@ describe('AppComponent', () => {
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(ProvidersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create an array of provider types', () => {
+    const configuredProviders = authModuleConfig.providers ?? [];
+
+    expect(component.providerTypes.length).toBe(configuredProviders.length);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should check pending state', () => {
+    expect(component.isPending(NgxSocialAuthProviderType.Google)).toBeFalse();
+  });
+
   it('should check authentication state', () => {
-    expect(component.isLoggedIn(NgxSocialAuthProviderType.Google)).toBeFalse();
+    expect(component.isAuthorized(NgxSocialAuthProviderType.Google)).toBeFalse();
   });
 
   it('should get auth state through the auth service', () => {
@@ -56,5 +95,18 @@ describe('AppComponent', () => {
 
     expect(socialAuthServiceSpy.signOut).toHaveBeenCalled();
   });
+
+  it('should track providers', () => {
+    expect(component.trackProviderTypes(0)).toBe(0);
+  });
+
+  it('should show auth response', () => {
+    socialAuthServiceSpy.signIn.and.returnValue(of({providerResponse: {}}));
+
+    component.onSignIn(NgxSocialAuthProviderType.Google);
+
+    component.showAuthResponse(NgxSocialAuthProviderType.Google);
+
+    expect(matBottomSheetSpy.open).toHaveBeenCalled();
+  });
 });
-*/
