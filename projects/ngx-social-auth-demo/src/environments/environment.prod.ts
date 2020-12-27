7a32c9162f7fbddbfb3244c6de0cead7ce754816
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-27 18:50:33 +0200

    build(demo): update microsoft prod environment

--- projects/ngx-social-auth-demo/src/environments/environment.prod.ts
+++ projects/ngx-social-auth-demo/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   googleClientId: '422761057421-32ts59dkd6moeqbelbkk44oh00jkf88b.apps.googleusercontent.com',
   facebookAppId: '1119074078473230',
-  microsoftClientId: 'f7ce20c8-7ccd-4ecb-95cb-8b7edb52be67',
+  microsoftClientId: '3e46b8b5-7756-452c-b6b2-d744f7fe6056',
   microsoftPostLogoutRedirect: 'https://ngx-social-auth2.web.app/'
 };
