37333edb188dc170d701fbd96d06b35e25d25e9b
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-27 14:50:17 +0200

    test(demo): test for ProviderNameService

---
+++ projects/ngx-social-auth-demo/src/app/core/provider-name.service.spec.ts
@@ -0,0 +1,25 @@
+import {TestBed} from '@angular/core/testing';
+import {ProviderNameService} from './provider-name.service';
+import {NgxSocialAuthProviderType} from 'ngx-social-auth2';
+
+describe('ProviderNameService', () => {
+  let service: ProviderNameService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ ProviderNameService ]
+    });
+
+    service = TestBed.get(ProviderNameService);
+  });
+
+  it('should create', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should resolve provider name', () => {
+    expect(service.resolve(NgxSocialAuthProviderType.Google)).toBe('Google');
+    expect(service.resolve(NgxSocialAuthProviderType.Facebook)).toBe('Facebook');
+    expect(service.resolve(NgxSocialAuthProviderType.Microsoft)).toBe('Microsoft');
+  });
+});
