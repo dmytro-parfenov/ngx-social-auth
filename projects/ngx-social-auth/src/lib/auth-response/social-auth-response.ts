4f98436addc1ad72aa532e4ebb55a467d4f113c2
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-27 15:16:11 +0200

    feat(lib): make readonly properties for the interfaces

--- projects/ngx-social-auth/src/lib/auth-response/social-auth-response.ts
+++ projects/ngx-social-auth/src/lib/auth-response/social-auth-response.ts
@@ -5,5 +5,5 @@ import {NgxSocialAuthProviderType} from '../social-auth-provider-type.enum';
  * Represents an object contains auth response
  */
 export interface NgxSocialAuthResponse<K extends NgxSocialAuthProviderType = NgxSocialAuthProviderType> {
-  providerResponse: SocialAuthResponseMap[K];
+  readonly providerResponse: SocialAuthResponseMap[K];
 }
--- projects/ngx-social-auth/src/lib/social-auth-module-config.ts
+++ projects/ngx-social-auth/src/lib/social-auth-module-config.ts
@@ -4,5 +4,5 @@ import {NgxSocialAuthProvider} from './provider/social-auth-provider';
  * Configuration for the {@link NgxSocialAuthModule}
  */
 export interface NgxSocialAuthModuleConfig {
-  providers?: NgxSocialAuthProvider[];
+  readonly providers?: NgxSocialAuthProvider[];
 }
